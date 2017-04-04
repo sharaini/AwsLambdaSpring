0b029465173e5db663b20235a69c3a00db6e34f6
sharath <sharath@Sharaths-MacBook-Air.local>
2017-04-04 10:47:47 -0400

    AWS lambda

---
+++ HelloLambda/src/main/java/com/amazonaws/lambda/example/HelloResponse.java
@@ -0,0 +1,20 @@
+package com.amazonaws.lambda.example;
+
+public class HelloResponse {
+	private String hello;
+
+    public HelloResponse(String hello) {
+        this.hello = hello;
+    }
+
+    public HelloResponse() {
+    }
+
+    public String getHello() {
+        return hello;
+    }
+
+    public void setHello(String hello) {
+        this.hello = hello;
+    }
+}
